86388b55986e383f56a04eac9dfa41de16495cc9
ExiqonBot <155193519+ExiqonBot@users.noreply.github.com>
2024-03-04 17:47:20 +0100

    Update levelling.js

--- Gallery/lib/levelling.js
+++ Gallery/lib/levelling.js
@@ -1,8 +1,9 @@
-function xpRange(level, multiplier = global.multiplier || 1) {
+export const growth = Math.pow(Math.PI / Math.E, 1.618) * Math.E * .75
+export function xpRange(level, multiplier = global.multiplier || 1) {
     if (level < 0)
         throw new TypeError('level cannot be negative value')
     level = Math.floor(level)
-    let min = level === 0 ? 0 : Math.round(Math.pow(level, growth) * multiplier) + 50
+    let min = level === 0 ? 0 : Math.round(Math.pow(level, growth) * multiplier) + 1
     let max = Math.round(Math.pow(++level, growth) * multiplier)
     return {
         min,
@@ -10,7 +11,7 @@ function xpRange(level, multiplier = global.multiplier || 1) {
         xp: max - min
     }
 }
-function findLevel(xp, multiplier = global.multiplier || 1) {
+export function findLevel(xp, multiplier = global.multiplier || 1) {
     if (xp === Infinity)
         return Infinity
     if (isNaN(xp))
@@ -23,7 +24,7 @@ function findLevel(xp, multiplier = global.multiplier || 1) {
     while (xpRange(level, multiplier).min <= xp)
     return --level
 }
-function canLevelUp(level, xp, multiplier = global.multiplier || 1) {
+export function canLevelUp(level, xp, multiplier = global.multiplier || 1) {
     if (level < 0)
         return false
     if (xp === Infinity)
